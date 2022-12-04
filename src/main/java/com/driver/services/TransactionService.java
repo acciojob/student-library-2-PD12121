644f8e80fa775a2945176d47d279b543efdd29b1
MyProjects <deshpandepranav47@gmail.com>
2022-12-04 17:47:21 +0530

    changes in transactionServiceIssueBook

--- src/main/java/com/driver/services/TransactionService.java
+++ src/main/java/com/driver/services/TransactionService.java
@@ -87,7 +87,7 @@ public class TransactionService {
             }
             transactionRepository5.save(transaction);
 
-            return transaction.getTransactionId();
+            return transaction.getTransactionId().toString();
         }
          //return transactionId instead
     }
